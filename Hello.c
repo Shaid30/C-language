53524c95e7e9ac36947598eb606353c05f9f4148
Abu Shaid <mdabushaid847@gmail.com>
2026-06-19 11:42:41 +0600

    updated

--- Hello.c
+++ Hello.c
@@ -1,6 +1,14 @@
 # include<stdio.h>
 
 int main() {
-    printf("Hello World!");
+    int a, b;
+    printf("enter a");
+    scanf("%d", &a);
+
+    printf("enter b");
+    scanf("%d", &b); 
+
+    int sum= a+b;
+    printf("sum is : %d", sum);
     return 0;
 }
